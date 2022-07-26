3b897a0d27cc2ec99013e0c77a93d8714cc339e3
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-07-26 16:26:30 +0530

    Add files via upload

---
+++ range2.py
@@ -0,0 +1,28 @@
+# n=int(input("enter any integer number:"))
+# for i in range(25,n):
+#     print(i,end=" ")
+# n=int(input("enter any integer number:"))
+# for i in range(2,n,2):
+#     print(i,end=" ")
+# b=int(input("enter any integer number:"))
+# if(b%2==0):
+#     print(f"{b} even number")
+# else:
+#     print(f"{b} odd number")
+# for i in range(1,5):
+    # print(i*i)
+    # print(i*i*i)
+    # print(i**3)
+# a=5
+# print(a)     #5
+# a+=2
+# print(a)        #7
+# a-=1
+# print(a)    #6
+# n=int(input("enter any integer number:"))
+# while(n<10):
+#     print("hello",str(n))
+#     n+=1
+#     n-=1
+
+
